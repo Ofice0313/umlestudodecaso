80aef2d949aa31482783f17913e2e8a4b396a2aa
Marcelo Caleb Julio Ofice <marcelocalebjulioo@gmail.com>
2025-01-16 22:53:44 +0200

    database H2 criated

--- src/main/java/com/caleb/umlestudodecaso/domain/Category.java
+++ src/main/java/com/caleb/umlestudodecaso/domain/Category.java
@@ -3,12 +3,20 @@ package com.caleb.umlestudodecaso.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+@Entity
 public class Category implements Serializable{
 	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
