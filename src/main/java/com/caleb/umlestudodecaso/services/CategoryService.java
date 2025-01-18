94f506ce97bda0ec916fe550966614b6955cecc2
Marcelo Caleb Julio Ofice <marcelocalebjulioo@gmail.com>
2025-01-18 02:46:12 +0200

    Adjustments on endpoint /categories/{id}

--- src/main/java/com/caleb/umlestudodecaso/services/CategoryService.java
+++ src/main/java/com/caleb/umlestudodecaso/services/CategoryService.java
@@ -1,12 +1,12 @@
 package com.caleb.umlestudodecaso.services;
 
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.caleb.umlestudodecaso.repositories.CategoryRepository;
 import com.caleb.umlestudodecaso.domain.*;
+import com.caleb.umlestudodecaso.services.exception.*;
 
 @Service
 public class CategoryService {
@@ -15,7 +15,8 @@ public class CategoryService {
 	private CategoryRepository repository;
 	public Category find(Integer id) {
 		Optional<Category> obj = repository.findById(id);
-		return obj.orElse(null);
+		return  obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Object not found! Id: " + id + ", kind: " + Category.class.getName()));
 	}
 
 }
---
+++ src/main/java/com/caleb/umlestudodecaso/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,22 @@
+package com.caleb.umlestudodecaso.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+	public ObjectNotFoundException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+	
+	
+	
+	
+	
+
+}
